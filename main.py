4f8ece8551d642f509381919216dacc3fb820c2c
Rohit De <de.rohit01@gmail.com>
2021-10-01 09:08:45 -0700

    added name in search box

--- main.py
+++ main.py
@@ -12,28 +12,6 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-
-# connects /kangaroos path to render kangaroos.html
-# @app.route('/kangaroos/')
-# def kangaroos():
-#     return render_template("kangaroos.html")
-#
-#
-# @app.route('/walruses/')
-# def walruses():
-#     return render_template("walruses.html")
-
-
-# @app.route('/hawkers/')
-# def hawkers():
-#     return render_template("hawkers.html")
-
-
-# @app.route('/stub/')
-# def stub():
-#     return render_template("stub.html")
-
-
 @app.route('/aadit', methods=['GET', 'POST'])
 def aadit():
     if request.form:
